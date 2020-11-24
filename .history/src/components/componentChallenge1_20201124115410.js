76b0b7fb4d65f4025ce79f0b0a9c934b011046ab
Jeff Walsh <jeffpwalsh@gmail.com>
2020-11-24 12:31:49 +0200

    commit

---
+++ .history/src/components/componentChallenge1_20201124115410.js
@@ -0,0 +1,122 @@
+import { indexOf } from 'lodash';
+import React, { useState } from 'react';
+
+import dataJson from '../data.json';
+
+function ComponentChallenge1() {
+  const [posts, setPosts] = useState([]);
+  const [responseType, setResponseType] = useState('');
+  const [contentType, setContentType] = useState('');
+  const [fetchStatus, setFetchStatus] = useState('');
+  const [toggleOn, setToggleOn] = useState(false);
+
+  const handleFetch = async () => {
+    // const response = await fetch(
+    //   '../../public/data.json'
+    // );
+    const response = await fetch(
+      'https://cors-anywhere.herokuapp.com/https://ambrite.ch/challenge/geo.json'
+    );
+    const response2 = await fetch(
+      'https://cors-anywhere.herokuapp.com/https://ambrite.ch/challenge/data.json'
+    );
+
+    // const data = dataJson
+    const data = await response.json();
+    const data2 = await response2.json();
+
+    const arr1 = data
+    const arr2 = data2
+    const arr3 = arr1.map((item, i) => Object.assign({}, item, arr2[i]));
+    // console.log(response);
+    // console.log(response2);
+    setResponseType(response.type === response2.type ? response.type : 'error');
+    setContentType(
+      response.headers.get('Content-Type') ===
+        response2.headers.get('Content-Type')
+        ? response.headers.get('Content-Type')
+        : 'error'
+    );
+    setFetchStatus(
+      response.status === response2.status ? response.status : 'error'
+    );
+
+    
+    setToggleOn(true);
+    console.log(contentType);
+    setPosts(arr3);
+    console.log(arr3)
+
+    const qty = arr3.map(item => item)
+    console.log(qty.length)
+
+    // data.forEach((item) => {
+    //   if (
+    //     typeof item.active === 'number' &&
+    //     typeof item.asn === 'number' &&
+    //     typeof item.id === 'number' &&
+    //     typeof item.countrycode === 'string' &&
+    //     typeof item.statecode === 'object'
+    //   ) {
+    //     console.log('data type is correct');
+    //   } else if (item.statecode === 'null') {
+    //     console.log('datatype is null');
+    //   }
+    // });
+
+    const arrErrors = [];
+
+    for (let i = 0; i < qty.length; i++) {
+      if (
+        (typeof qty[i].active === 'number' && qty[i].active !== 0) || qty[i].active === null)
+{
+  
+        console.log(`Array Object Index ${i} : All data types are correct`);
+      } else {
+        arrErrors.push(qty[i])
+        console.log(`Array Object Index ${i} : All data types are not correct`);
+      
+      }}
+    coole.log(arrErrors);
+  }
+
+  return (
+    <div>
+      <button onClick={handleFetch}>Continue</button>
+      <br />
+      <hr />
+      <div className={toggleOn ? 'Visible' : 'Hidden'}>
+        <h3>
+          Checking Response Type: <span className='type'>{responseType}</span>{' '}
+          <span className='validator'>
+            {responseType === 'cors' ? 'Check server proxy port' : 'Valid'}
+          </span>
+        </h3>
+        <hr />
+        <h3>
+          Checking Content Type: <span className='type'>{contentType}</span>{' '}
+          <span className='validator'>
+            {contentType === 'application/json' ? 'Valid' : 'Invalid'}
+          </span>
+        </h3>
+        <hr />
+        <h3>
+          Checking Status Code: <span className='type'>{fetchStatus}</span>{' '}
+          <span className='validator'>
+            {' '}
+            {fetchStatus === 200 ? 'Valid' : 'Invalid'}
+          </span>
+        </h3>
+      </div>
+
+      {posts.map((item) => (
+        <li key={item.id}>
+          {item.active} - {item.asn} - {item.id} - {item.countrycode} -{' '}
+          {item.statecode}
+        </li>
+      ))}
+    </div>
+  );
+}
+
+export default ComponentChallenge1;
--- src/components/ComponentChallenge2.js
+++ src/components/ComponentChallenge2.js
@@ -3,60 +3,92 @@ import Data from '../geo.json';
 import { sortBy, groupBy, memoize } from 'lodash';
 
 function ComponentChallenge2(props) {
+  const [posts, setPosts] = useState([]);
+  const [posts2, setPosts2] = useState([]);
   const [longitude, setLongitude] = useState('');
   const [latitude, setLatitude] = useState('');
   const [location, setLocation] = useState('');
   const [diff, setDiff] = useState('');
   const [adjust, setAdjust] = useState('');
-  const [long, setLong] = useState(Data)
-  const [lat, setLat] = useState(Data)
+  const [long, setLong] = useState(Data);
+  const [lat, setLat] = useState(Data);
   let splitGeo2 = [];
   let splitGeo3 = [];
 
+  // useEffect(() => {
+  //   Data.sort(function (a, b) {
+  //     return parseInt(b.geo) - parseInt(a.geo);
+  //   });
 
-  useEffect(() => {
-    Data.sort(function (a, b) {
-      return parseInt(b.geo) - parseInt(a.geo);
-    });
+  //   const geoArray = Data.map((location) => location.geo);
+  //   console.log(geoArray)
 
-    const geoArray = Data.map((location) => location.geo);
-    console.log(geoArray)
+  //   let splitGeo = geoArray.toString().split(',');
+  //   // console.log(splitGeo);
+  //   // let splitGeo2 = [];
+  //   // let splitGeo3 = [];
+  //   for (let i = 0; i < splitGeo.length; i++) {
+  //     if (i % 2 == 0) {
+  //       splitGeo2.push(parseFloat(splitGeo[i]));
+  //     } else {
+  //       splitGeo3.push(parseFloat(splitGeo[i]))
+  //     }
+  //   }
 
+  //   // console.log(splitGeo2);
+  //   // console.log(splitGeo3);
 
-    let splitGeo = geoArray.toString().split(',');
-    // console.log(splitGeo);
-    // let splitGeo2 = [];
-    // let splitGeo3 = [];
-    for (let i = 0; i < splitGeo.length; i++) {
-      if (i % 2 == 0) {
-        splitGeo2.push(parseFloat(splitGeo[i]));
-      } else {
-        splitGeo3.push(parseFloat(splitGeo[i]))
-      }
-    }
+  //   // setLong(... long, splitGeo2)
+  //   // setLat(... lat, splitGeo3)
 
-    // console.log(splitGeo2);
-    // console.log(splitGeo3);
+  //   // console.log(long)
+  //   // console.log(lat)
 
+  //   let splitArr = groupBy(Data, 'geo');
+  //   let splitArr2 = splitArr;
 
+  //   // console.log(splitArr)
+  //   // console.log(Data.slice(0, 9));
+  //   // console.log(JSON.stringify(Data.slice(0, 9)));
 
-    // setLong(... long, splitGeo2)
-    // setLat(... lat, splitGeo3)
+  //   let test = JSON.stringify(Data.slice(0, 9));
+  // }, []);
 
-    // console.log(long)
-    // console.log(lat)
+  const handleFetch = async () => {
+    const response = await fetch(
+      'https://cors-anywhere.herokuapp.com/https://ambrite.ch/challenge/geo.json'
+    );
+    const response2 = await fetch(
+      'https://cors-anywhere.herokuapp.com/https://ambrite.ch/challenge/data.json'
+    );
+    const data = await response.json();
+    const data2 = await response2.json();
 
-    let splitArr = groupBy(Data, 'geo');
-    let splitArr2 = splitArr;
+    // console.log(response);
+    // console.log(data);
+    // console.log(response2);
+    // console.log(data2);
 
-    // console.log(splitArr)
-    // console.log(Data.slice(0, 9));
-    // console.log(JSON.stringify(Data.slice(0, 9)));
+    const arr1 = data
+    const arr2 = data2
+    const arr3 = arr1.map((item, i) => Object.assign({}, item, arr2[i]));
 
-    let test = JSON.stringify(Data.slice(0, 9));
-  }, []);
+    // let join5 = [];
+    // const join = data.map((item) => item.geo);
+    // const join4 = join.forEach((x) => join5.push(x));
 
-  const handleFetch = () => {
+    // const join2 = Object.entries(join);
+    // const join3 = Object.keys(join2);
+
+    // console.log(join);
+    // console.log(join2);
+    // // console.log(join3);
+    // console.log(join4)
+    // console.log(join5);
+    console.log(arr3)
+    setPosts(arr3);
+   
+
     if ('geolocation' in navigator) {
       console.log('Available');
       navigator.geolocation.getCurrentPosition(function (position) {
@@ -65,11 +97,9 @@ function ComponentChallenge2(props) {
 
         setLongitude(position.coords.longitude);
         setLatitude(position.coords.latitude);
-        setLocation(`${longitude.toString()},${latitude.toString()}`);
-        setLong(... long, splitGeo2)
-        setLat(... lat, splitGeo3)
-
-
+        // setLocation(`${longitude.toString()},${latitude.toString()}`);
+        // setLong(... long, splitGeo2)
+        // setLat(... lat, splitGeo3)
       });
     } else {
       console.log('Not available');
@@ -81,22 +111,22 @@ function ComponentChallenge2(props) {
       <button onClick={handleFetch}>Continue</button>
       <h1>Longitude{longitude}</h1>
       <h1>Latitude{latitude}</h1>
-      <h1>Location{location}</h1>
-      <div>
+      {/* <h1>Location{location}</h1> */}
+      <div className="table">
         <table border='2'>
           <tbody>
             <tr>
-              <th>Longitude</th>
-              <th>Latitude</th>
-              <th>Location</th>
+              <th>Co-ordinates</th>
+              <th>Country Code</th>
+              <th>Active</th>
               <th>Difference</th>
             </tr>
-            {long.map((item, i) => (
-              <tr key={i}>
+            {posts.map((item, index) => (
+              <tr key={index}>
                 <td>{item.geo}</td>
-                <td>{item}</td>
-                <td>{item}</td>
-                <td>{item}</td>
+                <td>{item.countrycode}</td>
+                <td>{item.active === 1 ? 'Yes' : 'No'}</td>
+                <td>{location}</td>
               </tr>
             ))}
           </tbody>
